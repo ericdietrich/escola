d53aaca6c06ae71a409a0465d715e9001039a8f7
Eric Dietrich <eric_stefanon@msn.com>
2023-09-19 08:21:33 -0300

    initial commit

---
+++ src/test/java/CPFTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CPFTest {
+    @Test
+    void NaoDeveriaCriarCPFsComValorInvalido(){
+        assertThrows(IllegalArgumentException.class, () -> new CPF(null));
+        assertThrows(IllegalArgumentException.class, () -> new CPF(""));
+        assertThrows(IllegalArgumentException.class, () -> new CPF("12345678912"));
+    }
+    @Test
+    void deveriaPermitirCriarCFPComNumeroValido (){
+        String numero = "123.456.789-01";
+        CPF cpf = new CPF(numero);
+        assertEquals(numero, cpf.getNumero());
+    }
+}
